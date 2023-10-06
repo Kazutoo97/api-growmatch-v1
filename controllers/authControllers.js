4d5046be90f5016bd3f4ae08babb073428d353b6
Kazutoo12 <kirigaya120897@gmail.com>
2023-10-06 21:50:25 +0700

    Update Validasi

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,10 +2,30 @@ import Users from "../models/UsersModel.js";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
+import Joi from "joi";
 
 export const registerUser = async (req, res, next) => {
-  const { firstName, lastName, email, password, dob, interest } = req.body;
   try {
+    const schema = Joi.object({
+      firstName: Joi.string().min(3).max(12).alphanum().required(),
+      lastName: Joi.string().required().allow(""),
+      email: Joi.string().email().required(),
+      password: Joi.string().pattern(
+        new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+        )
+      ),
+      dob: Joi.date().iso().required(),
+      interest: Joi.string().min(3).required(),
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    const { firstName, lastName, email, password, dob, interest } = value;
+
     //**check if user already registered*/
     const exist = await Users.findOne({ email });
     if (exist) {
@@ -60,7 +80,7 @@ export const loginUser = async (req, res, next) => {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
       secure: true,
-      sameSite: 'None',
+      sameSite: "None",
     });
 
     res.status(200).json({ message: "Login successfully", data: accessToken });
